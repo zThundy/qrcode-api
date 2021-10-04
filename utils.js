8118265eb9e3e40d5b335f9c6a01da0f05eacd2a
zThundy <antonioantonelli7@gmail.com>
2021-10-04 20:45:48 +0200

    removed {} lol

--- utils.js
+++ utils.js
@@ -5,9 +5,7 @@ const makeID = (length) => {
     var result = [];
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
-    }
+    for (var i = 0; i < length; i++) result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
     return result.join('');
 }
 
