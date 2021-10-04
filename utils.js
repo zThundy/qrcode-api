cf208d5f2df14392a11a6051354dc55b5e4ebf59
zThundy <antonioantonelli7@gmail.com>
2021-10-04 20:27:12 +0200

    added check of headers authorization type

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const tokens = require("./authorized.json")
+
 const makeID = (length) => {
     var result = [];
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -8,4 +10,28 @@ const makeID = (length) => {
     return result.join('');
 }
 
-module.exports = { makeID }
+const checkAuthType = (header) => {
+    if (header) {
+        var tokenFound = null
+        // check if an authorization field is present in the header
+        if (header.authorization) {
+            const authType = header.authorization.split(" ");
+            // check if authorzation type is a bearer token
+            if (authType[0] == "Bearer") tokenFound = authType[1];
+            // check if authorization type is a basic authorization
+            if (authType[0] == "Basic")
+                for (var token of tokens) {
+                    var newToken = btoa("token:" + token);
+                    if (newToken == authType[1]) {
+                        tokenFound = token;
+                        break;
+                    }
+                };
+        // check if an api token is given
+        } else if (header.token) tokenFound = header.token;
+    }
+    if (tokenFound && tokens.includes(tokenFound)) return true;
+    return false;
+}
+
+module.exports = { makeID, print, checkAuthType }
