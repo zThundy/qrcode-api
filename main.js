0f8b46ad3eaea1fcd0b681ff1e50beb67754fbf6
zThundy <antonioantonelli7@gmail.com>
2021-10-05 00:20:07 +0200

    added /:variable and /?value routes

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 const app = require("express")();
-const QRCode = require('qrcode');
-const { makeID, isInTimeout, checkAuthType } = require("./utils.js")
+const { makeID, isInTimeout, checkAuthType, generateQR } = require("./utils.js")
 
 const PORT = 80;
-const ID_LENGHT = 25;
 
 app.set('trust proxy', 1);
 app.set('view engine', 'ejs');
 
-app.get("*", (req, res) => {
+app.get("/", async (req, res) => {
     if (!isInTimeout(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
-        QRCode.toDataURL(makeID(ID_LENGHT), { quality: 1.0 }, (err, url) => {
-            if (err) console.err(err);
-            res.render('qrcode', { qrurl: url, error: false });
-        })
+        const qr = await generateQR(req.query.value)
+        res.render('qrcode', { qrurl: qr, error: false });
+        res.end();
     } else {
         res.render('qrcode', { qrurl: "", error: "You are making too many requests" });
         res.end();
     }
 })
 
-app.post("*", (req, res) => {
+app.get("/:variable", async (req, res) => {
+    if (!isInTimeout(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
+        if (req.params && req.params.variable) {
+            const qr = await generateQR(req.params.variable)
+            res.render('qrcode', { qrurl: qr, error: false });
+            res.end();
+        }
+    } else {
+        res.render('qrcode', { qrurl: "", error: "You are making too many requests" });
+        res.end();
+    }
+})
+
+app.post("*", async (req, res) => {
     if (checkAuthType(req.headers)) {
         if (!isInTimeout(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
-            var tmp_id = makeID(ID_LENGHT);
-            QRCode.toDataURL(tmp_id, { quality: 1.0 }, (err, url) => {
-                if (err) console.err(err);
-                res.json({ response: url, id: tmp_id, code: 200 });
-                res.end();
-            })
+            var tmp_id = makeID(25);
+            const qr = await generateQR(req.headers.value || tmp_id)
+            res.json({ code: 200, id: req.headers.value || tmp_id, response: qr });
+            res.end();
         } else {
             res.status(403).json({ code: 403, response: "You are making too many requests" });
             res.end();
